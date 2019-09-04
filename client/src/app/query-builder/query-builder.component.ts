e168a7759a94c847653834a04eb2f7615213f3b7
MirwaisAkrami <m7akrami770@gmail.com>
2019-09-04 16:26:07 +0430

    #33 Improve data filtering

--- client/src/app/query-builder/query-builder.component.ts
+++ client/src/app/query-builder/query-builder.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
+declare var $: any;
 
 @Component({
 	selector: 'app-query-builder',
@@ -11,7 +12,13 @@ export class QueryBuilderComponent implements OnInit {
 	dataSheets = [];
 	wSheetName: string;
 	workBookName: string;
-	columDataTypes = [];
+	columnDataTypes = [];
+	columnDataType = '';
+	selectedColumnIndex = 0;
+	columnNames = [];
+	action = 'Actions';
+	applyMultiple = false;
+	originalData;
 	constructor() { }
 
 	ngOnInit() {
@@ -46,6 +53,39 @@ export class QueryBuilderComponent implements OnInit {
 			/* save data */
 			this.data = <XLSX.AOA2SheetOpts>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
 			console.log('data is: ', this.data);
+			// Get column names
+			this.data[0].forEach(element => {
+				const obj = {
+					showColumn: true,
+					name: element
+				};
+				this.columnNames.push(obj);
+			});
+
+			// After taking out the columns remove the first element of the data
+			this.data.splice(0, 1);
+
+			// Parse data for better handling
+			const tempData = [];
+
+			this.data.forEach(element => {
+				const obj = {
+					showRow: true,
+					rowData: element
+				}
+				tempData.push(obj);
+			});
+
+			console.log('temp data: ', tempData);
+			
+
+			this.data = tempData;
+
+			// Save the original data before processing this data
+			this.originalData = this.data;
+
+			console.log('Updated data: ', this.data);
+
 			this.checkColumnDataType();
 
 		};
@@ -55,14 +95,14 @@ export class QueryBuilderComponent implements OnInit {
 	checkColumnDataType() {
 		let isInteger;
 		let isUndefined;
-		for (let i = 0; i < this.data[0].length; i++) {
+		for (let i = 0; i < this.data[0].rowData.length; i++) {
 			isInteger = true;
 			isUndefined = true;
 			for (let j = 1; j < this.data.length; j++) {
-				console.log('Item: ', this.data[j][i]);
-				if (this.data[j][i] !== undefined) {
+				console.log('Item: ', this.data[j].rowData[i]);
+				if (this.data[j].rowData[i] !== undefined) {
 					isUndefined = false;
-					if (!(/^[0-9]\d*(\.\d+)?$/).test(this.data[j][i])) {
+					if (!(/^[0-9]\d*(\.\d+)?$/).test(this.data[j].rowData[i])) {
 						isInteger = false;
 						break;
 					}
@@ -70,21 +110,75 @@ export class QueryBuilderComponent implements OnInit {
 
 			}
 			if (!isUndefined) {
-				isInteger ? this.columDataTypes.push('number') : this.columDataTypes.push('string');
+				isInteger ? this.columnDataTypes.push('number') : this.columnDataTypes.push('string');
 			} else {
-				this.columDataTypes.push('undefined');
+				this.columnDataTypes.push('undefined');
 			}
-			// isInteger = true;
-			// for (let j = 1; j < 11; j++) {
-			// 	if (!(/[+-]?([0-9]*[.])?[0-9]+/g).test(this.data[j][i])) {
-			// 		isInteger = false;
-			// 		break;
-			// 	}
-			// }
-			// isInteger ? this.columDataTypes.push('number') : this.columDataTypes.push('string');
 		}
 
-		console.log('Data Types: ', this.columDataTypes);
+		console.log('Data Types: ', this.columnDataTypes);
 	}
+
+	showFilters(dataType, index) {
+		this.columnDataType = dataType;
+		this.selectedColumnIndex = index;
+		this.action = 'Actions';
+		console.log('Column Data Type: ', this.columnDataType);
+	}
+
+	// Update the dataType of a column when users decides to do so
+	updateDataType(newDataType) {
+		console.log('dataTypes prior to change: ', this.columnDataTypes);
+
+		this.columnDataTypes[this.selectedColumnIndex] = newDataType;
+		console.log('dataTypes after the change: ', this.columnDataTypes);
+		this.columnDataType = newDataType;
+	}
+
+	/**
+	 *
+	 * @param filterAction What type of filter to apply
+	 * @param actionName Name of the filter action
+	 * @param isMultipleApplicable are multiple filters applicable for this filter
+	 */
+	setAction(filterAction, actionName, isMultipleApplicable) {
+
+		// reset the multiple filter
+		$('#multiple-filter').prop('checked', false);
+		this.applyMultiple = isMultipleApplicable;
+	}
+
+	testData() {
+		const indeces = [3, 4, 5, 6];
+		// code to fitler certain rows
+		this.data = this.data.filter(dt => indeces.includes(this.data.indexOf(dt)));
+
+
+		// code to filter certain columns
+		// const columnIndex = 3;
+		// this.data = this.data.map(element => {
+		// 	console.log('element: ', element);
+		// 	console.log('element index: ', element.rowData.indexOf(element.rowData[columnIndex]));
+		// 	element.rowData = element.rowData.filter(vl => element.rowData.indexOf(vl) !== columnIndex);
+		// 	return element;
+		// });
+		// this.data.splice(3, 1);
+
+		console.log('final data: ', this.data);
+	}
+
+	resetData() {
+		this.data = this.originalData;
+	}
+
+	toggleColumn(column) {
+		console.log(column);
+		$(column).closest('li').toggleClass('unselected');
+		const checkBox = $(column).closest('li').find('input');
+		if (!$(column).hasClass('c-box')) {
+			$(column).closest('li').find('input').prop('checked', !checkBox.prop('checked'));
+		}
+	}
+
 
 }
