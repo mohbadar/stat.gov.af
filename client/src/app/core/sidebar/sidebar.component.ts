b6e18d281b13d8f1cf63260c0f19dfc2cf8be0ea
MirwaisAkrami <m7akrami770@gmail.com>
2019-09-15 11:46:57 +0430

    #33

--- client/src/app/core/sidebar/sidebar.component.ts
+++ client/src/app/core/sidebar/sidebar.component.ts
@@ -21,80 +21,94 @@ export interface ChildrenItems {
 }
 
 //Menu Items
-export const ROUTES: RouteInfo[] = [{
-	path: '/dashboard',
-	title: 'Dashboards',
-	type: 'sub',
-	icontype: 'pe-7s-graph'
-}
-// , {
-// 	path: '/components',
-// 	title: 'Components',
-// 	type: 'sub',
-// 	icontype: 'pe-7s-plugin',
-// 	children: [
-// 		{ path: 'buttons', title: 'Buttons', ab: 'B' },
-// 		{ path: 'grid', title: 'Grid System', ab: 'GS' },
-// 		{ path: 'panels', title: 'Panels', ab: 'P' },
-// 		{ path: 'sweet-alert', title: 'Sweet Alert', ab: 'SA' },
-// 		{ path: 'notifications', title: 'Notifications', ab: 'N' },
-// 		{ path: 'icons', title: 'Icons', ab: 'I' },
-// 		{ path: 'typography', title: 'Typography', ab: 'T' }
-// 	]
-// }, {
-// 	path: '/forms',
-// 	title: 'Forms',
-// 	type: 'sub',
-// 	icontype: 'pe-7s-note2',
-// 	children: [
-// 		{ path: 'regular', title: 'Regular Forms', ab: 'RF' },
-// 		{ path: 'extended', title: 'Extended Forms', ab: 'EF' },
-// 		{ path: 'validation', title: 'Validation Forms', ab: 'VF' },
-// 		{ path: 'wizard', title: 'Wizard', ab: 'W' }
-// 	]
-// }, {
-// 	path: '/tables',
-// 	title: 'Tables',
-// 	type: 'sub',
-// 	icontype: 'pe-7s-news-paper',
-// 	children: [
-// 		{ path: 'regular', title: 'Regular Tables', ab: 'RT' },
-// 		{ path: 'extended', title: 'Extended Tables', ab: 'ET' },
-// 		{ path: 'datatables.net', title: 'Datatables.net', ab: 'DT' }
-// 	]
-// }, {
-// 	path: '/maps',
-// 	title: 'Maps',
-// 	type: 'sub',
-// 	icontype: 'pe-7s-map-marker',
-// 	children: [
-// 		{ path: 'google', title: 'Google Maps', ab: 'GM' },
-// 		{ path: 'fullscreen', title: 'Full Screen Map', ab: 'FSM' },
-// 		{ path: 'vector', title: 'Vector Map', ab: 'VM' }
-// 	]
-// }, {
-// 	path: '/charts',
-// 	title: 'Charts',
-// 	type: 'link',
-// 	icontype: 'pe-7s-graph1'
+export const ROUTES: RouteInfo[] = [
+	// {
+	// 	path: '/',
+	// 	title: 'Home',
+	// 	type: 'link',
+	// 	icontype: 'pe-7s-home'
 
-// }, {
-// 	path: '/calendar',
-// 	title: 'Calendar',
-// 	type: 'link',
-// 	icontype: 'pe-7s-date'
-// }, {
-// 	path: '/pages',
-// 	title: 'Pages',
-// 	type: 'sub',
-// 	icontype: 'pe-7s-gift',
-// 	children: [
-// 		{ path: 'user', title: 'User Page', ab: 'UP' },
-// 		{ path: 'login', title: 'Login Page', ab: 'LP' },
-// 		{ path: 'register', title: 'Register Page', ab: 'RP' },
-// 		{ path: 'lock', title: 'Lock Screen Page', ab: 'LSP' }
-// 	]
-// }
+	// }, 
+	{
+		path: '/dashboard',
+		title: 'Dashboards',
+		type: 'sub',
+		icontype: 'pe-7s-graph'
+	}, {
+		path: '/build-query',
+		title: 'Custom Chart',
+		type: 'link',
+		icontype: 'pe-7s-graph1'
+
+	}
+	// , {
+	// 	path: '/components',
+	// 	title: 'Components',
+	// 	type: 'sub',
+	// 	icontype: 'pe-7s-plugin',
+	// 	children: [
+	// 		{ path: 'buttons', title: 'Buttons', ab: 'B' },
+	// 		{ path: 'grid', title: 'Grid System', ab: 'GS' },
+	// 		{ path: 'panels', title: 'Panels', ab: 'P' },
+	// 		{ path: 'sweet-alert', title: 'Sweet Alert', ab: 'SA' },
+	// 		{ path: 'notifications', title: 'Notifications', ab: 'N' },
+	// 		{ path: 'icons', title: 'Icons', ab: 'I' },
+	// 		{ path: 'typography', title: 'Typography', ab: 'T' }
+	// 	]
+	// }, {
+	// 	path: '/forms',
+	// 	title: 'Forms',
+	// 	type: 'sub',
+	// 	icontype: 'pe-7s-note2',
+	// 	children: [
+	// 		{ path: 'regular', title: 'Regular Forms', ab: 'RF' },
+	// 		{ path: 'extended', title: 'Extended Forms', ab: 'EF' },
+	// 		{ path: 'validation', title: 'Validation Forms', ab: 'VF' },
+	// 		{ path: 'wizard', title: 'Wizard', ab: 'W' }
+	// 	]
+	// }, {
+	// 	path: '/tables',
+	// 	title: 'Tables',
+	// 	type: 'sub',
+	// 	icontype: 'pe-7s-news-paper',
+	// 	children: [
+	// 		{ path: 'regular', title: 'Regular Tables', ab: 'RT' },
+	// 		{ path: 'extended', title: 'Extended Tables', ab: 'ET' },
+	// 		{ path: 'datatables.net', title: 'Datatables.net', ab: 'DT' }
+	// 	]
+	// }, {
+	// 	path: '/maps',
+	// 	title: 'Maps',
+	// 	type: 'sub',
+	// 	icontype: 'pe-7s-map-marker',
+	// 	children: [
+	// 		{ path: 'google', title: 'Google Maps', ab: 'GM' },
+	// 		{ path: 'fullscreen', title: 'Full Screen Map', ab: 'FSM' },
+	// 		{ path: 'vector', title: 'Vector Map', ab: 'VM' }
+	// 	]
+	// }, {
+	// 	path: '/charts',
+	// 	title: 'Charts',
+	// 	type: 'link',
+	// 	icontype: 'pe-7s-graph1'
+
+	// }, {
+	// 	path: '/calendar',
+	// 	title: 'Calendar',
+	// 	type: 'link',
+	// 	icontype: 'pe-7s-date'
+	// }, {
+	// 	path: '/pages',
+	// 	title: 'Pages',
+	// 	type: 'sub',
+	// 	icontype: 'pe-7s-gift',
+	// 	children: [
+	// 		{ path: 'user', title: 'User Page', ab: 'UP' },
+	// 		{ path: 'login', title: 'Login Page', ab: 'LP' },
+	// 		{ path: 'register', title: 'Register Page', ab: 'RP' },
+	// 		{ path: 'lock', title: 'Lock Screen Page', ab: 'LSP' }
+	// 	]
+	// }
 ];
 
 @Component({
@@ -120,7 +134,7 @@ export class SidebarComponent {
 	ngOnInit() {
 		console.log('Sidebar component called');
 		let tempChildren: ChildrenItems[] = [];
-		
+
 		var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 		this.menuItems = ROUTES.filter(menuItem => menuItem);
 
