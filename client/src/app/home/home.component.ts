75dee5d6d2daacd323bc8ee699471c95ab3fc7a7
zafarshafaq <zafarshafaq34@gmail.com>
2019-09-24 16:08:54 +0430

    #40 initial page design

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input, OnInit, SimpleChange, SimpleChanges, ViewChild, ElementRef, OnChanges, AfterViewInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
 	selector: 'home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 	
 	constructor() { 
 		console.log("Home Component");
 	}
 
-	
+	ngOnInit() {
+		$('.carousel').carousel({
+			interval: 3000
+		});
+	}
 }
