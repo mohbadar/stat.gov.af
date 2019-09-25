8c2710e71792058d6fb4ad494cdc88b6e6809b04
zafarshafaq <zafarshafaq34@gmail.com>
2019-09-25 16:09:10 +0430

    #40 Layout completed

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -14,8 +14,24 @@ export class HomeComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		$('.carousel').carousel({
-			interval: 3000
+		// counter
+		$('.counter-count').each(function () {
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			}, {
+				duration: 5000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
 		});
+
+
 	}
+
+	
 }
+
+
+
