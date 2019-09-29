87b1b87c7c39eef43d23520e8d27f839922ecb3f
mohbadar <mohammadbadarhashimi@gmail.com>
2019-09-29 12:52:32 +0430

    user service is done

--- client/src/app/services/node/user.service.ts
+++ client/src/app/services/node/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
+const ROLE_BASE_URL = "/node-api/users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  loadAllRoles(): Observable<any> {
+    return this.httpClient.get(`${ROLE_BASE_URL}/all`);
+  }
+
+  loadRole(id:string): Observable<any>
+  {
+    return this.httpClient.get(`${ROLE_BASE_URL}/one/${id}`);
+  }
+
+  create(data): Observable<any> 
+  {
+    return this.httpClient.post(`${ROLE_BASE_URL}/create`, data);
+  }
+
+  delete(id:string): Observable<any> {
+    return this.httpClient.delete(`${ROLE_BASE_URL}/remove/${id}`);
+  }
+
+
+  update(data) :Observable<any> {
+    return this.httpClient.put(`${ROLE_BASE_URL}/create`, data);
+  }
+
+
 }
