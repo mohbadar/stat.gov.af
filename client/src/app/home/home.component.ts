3f763c61ad5eb8b7ad0be704ce82aa7590d0bc2b
zafarshafaq <zafarshafaq34@gmail.com>
2019-10-01 10:35:01 +0430

    #40 Dashobards slider layout set

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -90,6 +90,27 @@ export class HomeComponent implements OnInit {
 			this.datatablesService.callServiceCmpMethod(selectedLang);
 			// this.navbarRTL(selectedLang);
 		});
+
+		$('#myCarousel').carousel({
+			interval: 10000
+		  })
+		  
+		  $('.carousel .item').each(function(){
+			var next = $(this).next();
+			if (!next.length) {
+			  next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+			
+			if (next.next().length>0) {
+			  next.next().children(':first-child').clone().appendTo($(this));
+			}
+			else {
+				$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+			}
+		  });
+
+
 	}
 
 	ngAfterViewInit() {
